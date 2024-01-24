eb54f1c487ce6c215e46d06493f952d8ed8bf5b0
unknown <nghialoe46@gmail.com>
2024-01-24 15:11:24 +0700

    first commit

---
+++ ImageCameraManager/ImagePopupForm.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using MessageBox = System.Windows.MessageBox;
+using WinForms = System.Windows.Forms;
+
+namespace ImageCameraManager
+{
+    /// <summary>
+    /// Interaction logic for ImagePopupForm.xaml
+    /// </summary>
+    public partial class ImagePopupForm : Window
+    {
+        private readonly dynamic? _selectedItem;
+
+        public ImagePopupForm()
+        {
+            InitializeComponent();
+            _selectedItem = MainWindow.Instance.SelectedItem as dynamic;
+            LoadImage();
+        }
+
+        private void LoadImage()
+        {
+            try
+            {
+                var getPath = _selectedItem?.Path;
+
+                if (string.IsNullOrEmpty(getPath))
+                {
+                    MessageBox.Show("Selected item path is null or empty.", "Error");
+                    return;
+                }
+
+                string fileNameWithoutExtension = GetFileNameWithoutExtension(getPath);
+
+                SetNameWithoutExtension(fileNameWithoutExtension);
+
+                BitmapImage bitmap = LoadBitmapImage(getPath);
+
+                SetImageSource(bitmap);
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show($"Error loading image: {ex.Message}", "Error");
+            }
+            catch (UriFormatException ex)
+            {
+                MessageBox.Show($"Error loading image: {ex.Message}", "Error");
+            }
+        }
+
+        private string GetFileNameWithoutExtension(string path)
+        {
+            return Path.GetFileNameWithoutExtension(path);
+        }
+
+        private void SetNameWithoutExtension(string name)
+        {
+            NameTxt.Text = name;
+        }
+
+        private BitmapImage LoadBitmapImage(string path)
+        {
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(path, UriKind.RelativeOrAbsolute);
+            bitmap.EndInit();
+            return bitmap;
+        }
+
+        private void SetImageSource(BitmapImage bitmap)
+        {
+            ImageSrc.Source = bitmap;
+        }
+
+        private async void SubmitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Show a loading indicator or disable the button to indicate the operation is in progress
+            // YourLoadingIndicator.Visibility = Visibility.Visible;
+            // SubmitBtn.IsEnabled = false;
+
+            await Task.Run(() => RenameSelectedItem());
+
+            // Hide the loading indicator or enable the button once the operation is complete
+            // YourLoadingIndicator.Visibility = Visibility.Collapsed;
+            // SubmitBtn.IsEnabled = true;
+
+            this.Close();
+        }
+
+        private void RenameSelectedItem()
+        {
+            string newName = NameTxt.Text.Trim();
+
+            if (string.IsNullOrEmpty(newName))
+                return;
+
+            try
+            {
+                // Get the directory of the selected item
+                string parentDirectory = Path.GetDirectoryName(_selectedItem?.Path);
+
+                if (string.IsNullOrEmpty(parentDirectory))
+                {
+                    MessageBox.Show("Parent directory is null or empty.", "Error");
+                    return;
+                }
+
+                // Create the new path with the new name using Path.Combine
+                string newPath = Path.Combine(parentDirectory, newName);
+
+                // Check if the file exists before attempting to move it
+                if (File.Exists(_selectedItem?.Path))
+                {
+                    // Perform the renaming operation for files
+                    File.Move(_selectedItem.Path, newPath);
+                }
+                else
+                {
+                    MessageBox.Show("The file does not exist.", "Error");
+                }
+
+                // Update the UI in the main window after renaming
+                MainWindow.Instance?.UpdateFileList(parentDirectory);
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show($"Error renaming item: {ex.Message}", "Error");
+            }
+        }
+    }
+}
---
+++ ImageCameraManager/MainWindow.xaml.cs
@@ -0,0 +1,208 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using WinForms = System.Windows.Forms;
+
+
+namespace ImageCameraManager
+{
+    public partial class MainWindow
+    {
+        private FilterInfoCollection? _filterInfoCollection = null;
+        private VideoCaptureDevice? _videoCaptureDevice;
+        private readonly Stack<string> _folderHistory = new Stack<string>();
+        private readonly Stack<string> _forwardHistory = new Stack<string>();
+        private readonly FileSystemWatcher _fileSystemWatcher = new FileSystemWatcher();
+        public static MainWindow? Instance { get; private set; } 
+        public dynamic? SelectedItem => ListView.SelectedItem as dynamic;
+        public MainWindow()
+        {
+            InitializeComponent();
+            UpdateButtonStates();
+            Loaded += MainWindow_Loaded;
+            Closing += MainWindow_Closing!;
+            Instance = this;
+        }
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            _filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (_filterInfoCollection.Count <= 0) return;
+            _videoCaptureDevice = new VideoCaptureDevice(_filterInfoCollection[0].MonikerString);
+            _videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+            _videoCaptureDevice.Start();
+        }
+
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            webcamPictureBox.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (_videoCaptureDevice is { IsRunning: true })
+            {
+                _videoCaptureDevice.SignalToStop();
+                _videoCaptureDevice.WaitForStop();
+            }
+
+            _fileSystemWatcher.Dispose();
+        }
+
+        private void TackePicBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (webcamPictureBox.Image != null)
+            {
+                var snapshot = (Bitmap)webcamPictureBox.Image.Clone();
+                var currentFolder = _folderHistory.Peek();
+                var fileName = $"selfie_{DateTime.Now:yyyyMMddHHmmss}.png";
+                var filePath = Path.Combine(currentFolder, fileName);
+                snapshot.Save(filePath, System.Drawing.Imaging.ImageFormat.Png);
+                System.Windows.MessageBox.Show($"Snapshot saved to {filePath}", "Selfie Saved", MessageBoxButton.OK, MessageBoxImage.Information);
+                UpdateFileList(_folderHistory.Peek());
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("No frame captured.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void BrowseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedFolder = BrowseForFolder();
+            if (string.IsNullOrEmpty(selectedFolder)) return;
+
+            _folderHistory.Push(selectedFolder);
+            _forwardHistory.Clear();
+
+            UpdateFileList(selectedFolder);
+            UpdateButtonStates();
+        }
+
+        private string? BrowseForFolder()
+        {
+            var dialog = new FolderBrowserDialog();
+            dialog.SelectedPath = @"";
+            var result = dialog.ShowDialog();
+            UpdateFilePathIntoTextBox(dialog.SelectedPath);
+            return result == WinForms.DialogResult.OK ? dialog.SelectedPath : null;
+        }
+
+        private void UpdateFilePathIntoTextBox(string path)
+        {
+            DirectoryTxt.Text = !string.IsNullOrWhiteSpace(path) ? path : "Invalid Path";
+        }
+        private void ForwardBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (_forwardHistory.Count <= 0) return;
+            var forwardFolder = _forwardHistory.Pop();
+            _folderHistory.Push(forwardFolder);
+            UpdateFileList(forwardFolder);
+            UpdateFilePathIntoTextBox(forwardFolder);
+            UpdateButtonStates();
+        }
+
+        private void UpdateButtonStates()
+        {
+            TackePicBtn.IsEnabled = DirectoryTxt.Text != string.Empty;
+            BackwardBtn.IsEnabled = _folderHistory.Count > 1;
+            ForwardBtn.IsEnabled = _forwardHistory.Count > 0;
+        }
+        private void BackwardBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (_folderHistory.Count <= 1) return;
+            var currentFolder = _folderHistory.Pop();
+            _forwardHistory.Push(currentFolder);
+
+            var previousFolder = _folderHistory.Peek();
+            UpdateFileList(previousFolder);
+            UpdateFilePathIntoTextBox(previousFolder);
+            UpdateButtonStates();
+        }
+
+        public void UpdateFileList(string? selectedFolder)
+        {
+            if (selectedFolder == null) return;
+
+            var entries = Directory.GetFileSystemEntries(selectedFolder); 
+            var fileInfos = entries.Select(entry => CreateFileInfo(entry))
+                                   .Where(item => item.Type == "Folder" || item.Type == "Image")
+                                   .OrderByDescending(item => item.Type == "Folder").ToList();
+
+            ListView.ItemsSource = fileInfos;
+        }
+        private static bool IsImageFile(string? extension)
+        {
+            string[] imageExtensions = { ".jpg", ".jpeg", ".png", ".gif", ".bmp" };
+            return imageExtensions.Contains(extension, StringComparer.OrdinalIgnoreCase);
+        }
+        private static dynamic CreateFileInfo(string entry)
+        {
+            return new
+            {
+                Type = GetFileType(entry),
+                Name = Path.GetFileName(entry),
+                Path = entry,
+                Icon = GetFileIcon(entry)
+            };
+        }
+        private static string GetFileType(string path)
+        {
+            var extension = Path.GetExtension(path)?.ToLower();
+
+            if (File.GetAttributes(path).HasFlag(FileAttributes.Directory))
+            {
+                return "Folder";
+            }
+            else if (IsImageFile(extension))
+            {
+                return "Image";
+            }
+            else
+            {
+                return "File";
+            }
+        }
+        private void ListView_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (ListView.SelectedItem == null) return;
+            var selectedItem = (dynamic)ListView.SelectedItem;
+
+            if (selectedItem.Type == "Folder")
+            {
+                _folderHistory.Push(selectedItem.Path);
+                _forwardHistory.Clear();
+                _forwardHistory.Clear();
+
+                UpdateFilePathIntoTextBox(selectedItem.Path);
+                UpdateFileList(selectedItem.Path);
+                UpdateButtonStates();
+            }
+            else if (selectedItem.Type == "Image")
+            {
+                //OpenFile(selectedItem.Path);
+                ImagePopupForm imagePopupForm = new ImagePopupForm();
+                imagePopupForm.ShowDialog();
+            }
+        }
+
+        private static void OpenFile(string filePath)
+        {
+            var processStartInfo = new ProcessStartInfo(filePath)
+            {
+                UseShellExecute = true
+            };
+            Process.Start(processStartInfo);
+        }
+
+        private static BitmapImage GetFileIcon(string path)
+        {
+            var isDirectory = File.GetAttributes(path).HasFlag(FileAttributes.Directory);
+            var iconPath = isDirectory
+                ? @"D:\Repos\PRN221\ImageCameraManager\ImageCameraManager\Resources\folder.png"
+                : @"D:\Repos\PRN221\ImageCameraManager\ImageCameraManager\Resources\file.png";
+            return new BitmapImage(new Uri(iconPath));
+        }
+    }
+}
